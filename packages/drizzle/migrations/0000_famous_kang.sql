96eda12bf96983a7f8e17432baa8b2686d4a0f80
ririxi <dev@ririxi.dev>
2025-09-08 01:46:09 +0200

    feat(drizzle): add `uuidv7` function for pg17

--- packages/drizzle/migrations/0000_famous_kang.sql
+++ packages/drizzle/migrations/0000_famous_kang.sql
@@ -2,6 +2,21 @@
 CREATE EXTENSION IF NOT EXISTS pg_trgm;
 CREATE EXTENSION IF NOT EXISTS postgis;
 
+CREATE FUNCTION uuidv7() RETURNS uuid
+AS $$
+  -- Replace the first 48 bits of a uuidv4 with the current
+  -- number of milliseconds since 1970-01-01 UTC
+  -- and set the "ver" field to 7 by setting additional bits
+  select encode(
+    set_bit(
+      set_bit(
+        overlay(uuid_send(gen_random_uuid()) placing
+	  substring(int8send((extract(epoch from clock_timestamp())*1000)::bigint) from 3)
+	  from 1 for 6),
+	52, 1),
+      53, 1), 'hex')::uuid;
+$$ LANGUAGE sql volatile;
+
 -- CREATE OR REPLACE FUNCTION set_updated_at() RETURNS trigger AS $$
 -- BEGIN
 --   NEW."updatedAt" = NOW();
