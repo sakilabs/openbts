d2e2f20fda4ff761d4ebeda87da3ea2d99866296
ririxi <dev@ririxi.dev>
2025-12-23 20:19:39 +0100

    feat: use bun sql

--- packages/drizzle/db.ts
+++ packages/drizzle/db.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+import { SQL } from "bun";
 
 import * as schema from "./schemas/index.js";
 
@@ -9,10 +10,11 @@ if (!connectionString) {
 	throw new Error("DATABASE_URL environment variable is not set");
 }
 
-export const sql = postgres(connectionString, { idle_timeout: 20, max_lifetime: 60 * 30 });
+// export const sql = postgres(connectionString, { idle_timeout: 20, max_lifetime: 60 * 30 });
+const client = new SQL(connectionString as string);
 
 export const db = drizzle({
-	client: sql,
+	client,
 	schema,
 });
 
