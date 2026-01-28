324dd9b1267c585e1bb29e2c52a574f0268acc4c
ririxi <dev@ririxi.dev>
2026-01-28 03:26:02 +0100

    fix: disable react compiler
    TanStack Table does not work with it and "use no memo" does not work for some reason

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -1,4 +1,4 @@
-import path from "path";
+import { resolve } from "node:path";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { reactRouter } from "@react-router/dev/vite";
@@ -8,19 +8,21 @@ const ReactCompilerConfig = {};
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [reactRouter(), tailwindcss(), babel({
-		apply: "build",
-		filter: /\.[jt]sx?$/,
-		babelConfig: {
-		  presets: ["@babel/preset-typescript"],
-		  plugins: [
-			["babel-plugin-react-compiler", ReactCompilerConfig],
-		  ],
-		},
-	  }),],
+	plugins: [
+		reactRouter(),
+		tailwindcss(),
+		// babel({
+		// 	apply: "build",
+		// 	filter: /\.[jt]sx?$/,
+		// 	babelConfig: {
+		// 		presets: ["@babel/preset-typescript"],
+		// 		plugins: [["babel-plugin-react-compiler", ReactCompilerConfig]],
+		// 	},
+		// }),
+	],
 	resolve: {
 		alias: {
-			"@": path.resolve(__dirname, "./src"),
+			"@": resolve(__dirname, "./src"),
 		},
 	},
 	build: {
