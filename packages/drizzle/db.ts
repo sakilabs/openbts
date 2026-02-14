89635fa6c3257413cb2435ce7671dd7ba6893efd
ririxi <dev@ririxi.dev>
2026-02-14 22:29:55 +0100

    feat: disable prepared statements in `postgres.js` because we use pgbouncer

--- packages/drizzle/db.ts
+++ packages/drizzle/db.ts
@@ -9,7 +9,12 @@ if (!connectionString) {
 	throw new Error("DATABASE_URL environment variable is not set");
 }
 
-export const sql = postgres(connectionString, { idle_timeout: 20, max_lifetime: 60 * 30 });
+export const sql = postgres(connectionString, {
+	prepare: false,
+	max: 10,
+	idle_timeout: 20,
+	connect_timeout: 10,
+});
 
 export const db = drizzle({
 	client: sql,
