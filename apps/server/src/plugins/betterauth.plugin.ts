d5b5da3857dbffaa619fb6a91d2dbb09f0691e05
ririxi <dev@ririxi.dev>
2026-02-18 16:59:20 +0100

    feat(betterAuth): add cookie caching

--- apps/server/src/plugins/betterauth.plugin.ts
+++ apps/server/src/plugins/betterauth.plugin.ts
@@ -134,6 +134,10 @@ export const auth = betterAuth({
     enabled: false,
   },
   session: {
+    cookieCache: {
+      enabled: true,
+      maxAge: 5 * 60,
+    },
     expiresIn: 60 * 60 * 24 * 7, // 7 days
     updateAge: 60 * 60 * 24, // 1 day
   },
