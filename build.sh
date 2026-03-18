74e7d7b00821d89b818fa2ae1c1ebed0e551d665
ririxi <dev@ririxi.dev>
2026-03-19 00:00:32 +0100

    feat: replace logo

--- build.sh
+++ build.sh
@@ -3,4 +3,10 @@ set -e
 
 COMMIT_SHA=$(git rev-parse --short HEAD)
 
-COMMIT_SHA=$COMMIT_SHA docker compose build "$@"
+if [[ "$1" == "client" || "$1" == "server" ]]; then
+  SERVICE=$1
+  COMMIT_SHA=$COMMIT_SHA docker compose build "$SERVICE"
+  docker compose up -d --no-deps "$SERVICE"
+else
+  COMMIT_SHA=$COMMIT_SHA docker compose build "$@"
+fi
