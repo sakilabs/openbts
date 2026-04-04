143da4c3095a1575664cf3f853c629472ddb22de
ririxi <dev@ririxi.dev>
2026-04-05 01:14:50 +0200

    fix: swarm changes for proper build

--- build.sh
+++ build.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 set -e
 
-COMMIT_SHA=$(git log --invert-grep --grep="^feat: swap \`OpenBTS\` to \`BTSearch\`" -1 --format="%h")
+export COMMIT_SHA=$(git log --invert-grep --grep="^feat: swap \`OpenBTS\` to \`BTSearch\`" -1 --format="%h")
 
 if [[ "$1" == "swarm" ]]; then
   COMMIT_SHA=$COMMIT_SHA docker compose build client server
   docker build -t btsearch-postgres -f docker/postgres/Dockerfile .
+  set -a && source .env && set +a
+  docker stack deploy -c docker-compose.swarm.yml btsearch
+elif [[ "$1" == "deploy" ]]; then
+  export COMMIT_SHA=$(docker service inspect btsearch_server --format '{{index (split .Spec.TaskTemplate.ContainerSpec.Image ":") 1}}')
   set -a && source .env && set +a
   docker stack deploy -c docker-compose.swarm.yml btsearch
 elif [[ "$1" == "client" || "$1" == "server" ]]; then
