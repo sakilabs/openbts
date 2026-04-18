bdb028bda8d5282cd9c2eee8ccd5d0c8a7750bca
ririxi <dev@ririxi.dev>
2026-04-18 02:14:38 +0200

    fix(docker): build discord-bot

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e
 export COMMIT_SHA=$(git log --invert-grep --grep="^feat: swap \`OpenBTS\` to \`BTSearch\`" -1 --format="%h")
 
 if [[ "$1" == "swarm" ]]; then
-  COMMIT_SHA=$COMMIT_SHA docker compose build client server
+  COMMIT_SHA=$COMMIT_SHA docker compose build client server discord-bot
   docker build -t btsearch-postgres -f docker/postgres/Dockerfile .
   set -a && source .env && set +a
   docker stack deploy -c docker-compose.swarm.yml btsearch
