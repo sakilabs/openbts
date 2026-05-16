96b7b741890691558b36c2a7154c171c57c205b2
ririxi <dev@ririxi.dev>
2026-05-16 22:30:13 +0200

    fix(proto): include `status`

--- apps/server/src/hooks/onSend.hook.ts
+++ apps/server/src/hooks/onSend.hook.ts
@@ -6,7 +6,7 @@ export async function OnSendHook(req: FastifyRequest, res: FastifyReply, payload
 
   res.header("x-response-time", `${(Number(duration) / 1e6).toFixed(3)} ms`);
   const protoSchema = res.routeOptions.config.proto;
-  if (protoSchema && req.headers.accept === "application/x-protobuf" && typeof payload === "string") {
+  if (protoSchema && req.headers.accept === "application/x-protobuf" && typeof payload === "string" && res.statusCode < 400) {
     const encoded = toBinary(protoSchema, fromJson(protoSchema, JSON.parse(payload)));
     res.header("Content-Type", "application/x-protobuf");
     return Buffer.from(encoded);
